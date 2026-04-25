92bc31a86b88bdab73129f4a4c3ad097130a92a9
Minit <minitjain.work@gmail.com>
2026-04-26 03:14:54 +0530

    chore(day-25): add vercel.json, prod-ready env vars, cleanup dead files

--- Frontend/src/features/home/service/song.api.js
+++ Frontend/src/features/home/service/song.api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true
-})
-
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  withCredentials: true,
+});
 
 export async function getSong({ mood }) {
-    const response = await api.get("/api/songs?mood=" + mood)
-    console.log(response)
-    return response.data
+  const response = await api.get("/api/songs?mood=" + mood);
+  console.log(response);
+  return response.data;
 }
